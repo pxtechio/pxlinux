ca07ca650507889f95edb39cb864a273cd0bf255
Parallels <rhimmelbauer>
2019-04-18 15:52:43 -0700

    fixed docker ~

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --user=root -it --rm --privileged -v ./pxboard/base/assets:/assets -v ./pxboard/base/config:/config pxtech/pxlinux-ci:latest
+docker run --user=root -it --rm --privileged -v ~/pxboard/base/assets:/assets -v ~/pxboard/base/config:/config pxtech/pxlinux-ci:latest
