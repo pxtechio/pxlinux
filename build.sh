b7b276cacc5c3795be10ff577b07c4c9a3931f22
Parallels <rhimmelbauer>
2019-04-18 16:38:42 -0700

    zip

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ docker run --user=root -it --rm --privileged \
   -v /home/travis/build/pxtechio/pxlinux/pxboard/base/config:/config \
   pxtech/pxlinux-ci:latest
 
+sudo chown travis:travis /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img
 zip /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img.zip /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img
 
 sh -x upload_image.sh /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img.zip PXBoardQP4G-latest.img.zip
