8a575037ec2144397095387a8a53074bf653a748
Jose Ruiz <chepe.ruiz@gmail.com>
2019-07-25 15:21:16 -0700

    Added support for PRO img

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ docker run --user=root -it --rm --privileged  \
   -v /home/travis/build/pxtechio/pxlinux/pxboard/base/config:/config \
   pxtech/pxlinux-ci:latest
 
-sudo chown travis:travis /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img
-sudo rm /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img.zip
-sudo zip -j /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img.zip  /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img
+sudo chown travis:travis /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQ-latest.img
+sudo rm /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQ-latest.img.zip
+sudo zip -j /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQ-latest.img.zip  /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQ-latest.img
 
-sh -x upload_img.sh /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQP4G-latest.img.zip PXBoardQP4G-latest.img.zip
+sh -x upload_img.sh /home/travis/build/pxtechio/pxlinux/pxboard/base/assets/images/PXBoardQ-latest.img.zip PXBoardQ-latest.img.zip
